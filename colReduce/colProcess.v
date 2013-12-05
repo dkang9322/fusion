a2518bcda51c0662a689293c522da94282ff2699
dkang9322 <wkang@mit.edu>
2013-12-04 21:11:13 -0500

    Modified Wrapper such that we get higher intensity values. Not sure of its effect. Added bram_memory structure to display delayed color, colProcess now has its own bram running

--- colReduce/colProcess.v
+++ colReduce/colProcess.v
@@ -25,16 +25,47 @@ module colProc(reset, clk,
    wire [35:0] 	 two_proc_pixs;
    wire [18:0] 	 proc_pix_addr;
 
+   // Undelayed processed pixels, we need to hold the value
+   // for max_hcount - process_latency = 1344 - 24 = 1320
+   wire [35:0] 	 two_undel_proc_pixs;
+
    colWrapper col_abstr(reset, clk, two_pixel_vals,
-		       two_proc_pixs,
+		       two_undel_proc_pixs,
 		       switch_vals, switch_sels, change);
    
+
+   // For delaying by the latency, we use a BRAM
+   // We actually need only 1320/2=660 address in BRAM
+   // since we're storing two pixels per two-cycles
+   parameter LOGSIZE = 10;
+   parameter WIDTH = 36;
+
+   // Caution, this might be off, start writing on even hcount
+   wire       bram_we = ~hcount[0];
+
+   // So the BRAM structure is as follows
+   // It stores the col_proc_pixel as a row
+   // On one cycle, it writes the two_pixel_worth data
+   // On the other cycle, it reads from location hcount>>1+12
+   // Note that end effects are negligible, since we have power_of_2
+   // Also, we want to have different addresses for read/write
+   wire [LOGSIZE-1:0] bram_writ_addr=hcount>>1;
+   wire [LOGSIZE-1:0] bram_read_addr=(hcount>>1)+12;
+   wire [LOGSIZE-1:0] bram_addr = bram_we ? bram_writ_addr : bram_read_addr;
+   
+   // 1024 by 36 bits address delay
+   // Higher addresses are mostly junk, and unused
+   mybram #(.LOGSIZE(LOGSIZE), .WIDTH(WIDTH)) (bram_addr,
+					       clk,
+					       two_undel_proc_pixs,
+					       two_del_proc_pixs,
+					       bram_we);
+
    //forecast hcount & vcount 8 clock cycles ahead
    //Same as hcount_f/vcount_f in vram_display_module
    wire [10:0] 	 hcount_f = (hcount >= 1048) ? (hcount - 1048) : (hcount + 8);
    wire [9:0] vcount_f = (hcount >= 1048) ? ((vcount == 805) ? 0 : vcount + 1) : vcount;
 
-   
    assign proc_pix_addr = {vcount_f, hcount_f[9:1]};
 
 endmodule // colProc
--- colReduce/colWrapper.v
+++ colReduce/colWrapper.v
@@ -22,8 +22,8 @@ module colWrapper(reset, clk,
    // Input to colorReduce
    wire [23:0] 	 pix1RGB, pix2RGB;
    
-   assign pix1RGB = {two_pixel_vals[17:12], 2'b0, two_pixel_vals[11:6], 2'b0, two_pixel_vals[5:0], 2'b0};
-   assign pix2RGB = {two_pixel_vals[17+18:12+18], 2'b0, two_pixel_vals[11+18:6+18], 2'b0, two_pixel_vals[5+18:0+18], 2'b0};
+   assign pix1RGB = {two_pixel_vals[17:12], 2'b11, two_pixel_vals[11:6], 2'b11, two_pixel_vals[5:0], 2'b11};
+   assign pix2RGB = {two_pixel_vals[17+18:12+18], 2'b11, two_pixel_vals[11+18:6+18], 2'b11, two_pixel_vals[5+18:0+18], 2'b11};
 
    // Output to colorReduce
    wire [23:0] 	 tRGB1, tRGB2;
